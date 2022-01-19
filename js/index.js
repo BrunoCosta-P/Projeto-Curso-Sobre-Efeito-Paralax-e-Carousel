4bd297a96664b9d124f12b0331c3c7c53b7804ba
Bruno Costa Pinto <brunocosta524@hotmail.com>
2022-01-19 20:45:01 -0300

    Primeiro Commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { Carrossel } from "./carrossel.js";
+
+const anterior = '[data-anterior]'
+
+const proximo = '[data-proximo]'
+
+const listaProdutos = '[data-lista-produtos]'
+
+const navegacao = '[data-navegacao]'
+
+
+new Carrossel(anterior, proximo, listaProdutos, navegacao)
+
